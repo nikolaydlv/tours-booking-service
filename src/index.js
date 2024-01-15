4e6e9e55261ffac872aceb6b0fd1f220f1843e80
Nikolay <wokewoke@mail.ru>
2024-01-15 09:51:20 +0300

    add webpack and resctructuring project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './styles/styles.css';
+import "./libs/autoComplete/css/autoComplete.01.css";
+import "./libs/custom-select/custom-select.css";
+
+
+customSelect('select');
+
+ const picker = new easepick.create({
+        lang: 'ru-Ru',
+		element: document.getElementById('date'),
+		css: [
+			'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+		],
+		format: 'DD.MM.YY',
+ });
+ 
+ /* Subscribe Label */
+
+ const inputSubscribe = document.querySelector('.subscribe__input');
+ const label = document.querySelector('.subscribe__label');
+ inputSubscribe.addEventListener('input', () => {
+	if (inputSubscribe.value.trim() !== '') {
+		label.classList.add('.subscribe__label--top')
+	} else {
+		label.classList.remove('.subscribe__label--top')
+	}
+ })
